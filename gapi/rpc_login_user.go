c98129b9c2c91cf61dbda882862c7cb8d9233dd7
MathPeixoto <matheus-pxt@hotmail.com>
2023-02-01 00:10:58 -0300

    extracting grpc and http metadata from context

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -36,14 +36,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
-	//p, _ := peer.FromContext(ctx)
+	metadata := server.extractMedatada(ctx)
 
 	arg := db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    metadata.UserAgent,
+		ClientIp:     metadata.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiresAt,
 	}
